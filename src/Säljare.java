9df1fcf17c4ad7347febf30bbd128c33a07d45a2
Filip Fagerström <filip.fagerstrom@elev.ga.ntig.se>
2026-01-19 10:17:42 +0100

    Implement employee subclasses with specific work methods and attributes

--- src/Säljare.java
+++ src/Säljare.java
@@ -1,8 +1,13 @@
 public class Säljare extends Anställd {
     private int antalSåldaEnheter;
 
-    public Säljare(String namn, int måndaslön) {
+    public Säljare(String namn, int månadslön) {
         super(namn, månadslön);
         this.antalSåldaEnheter = 0;
+    }
+
+    public void arbete() {
+        System.out.println(namn + " har sålt " + antalSåldaEnheter + " enheter");
+        antalSåldaEnheter += 10;
     }
 }
--- src/Utvecklare.java
+++ src/Utvecklare.java
@@ -1,3 +1,23 @@
 public class Utvecklare extends Anställd {
-    
+    private String programmeringsSpråk;
+    private int antalRaderKod;
+
+    public Utvecklare(String namn, int månadslön, String programmeringsSpråk) {
+        super(namn, månadslön);
+        this.programmeringsSpråk = programmeringsSpråk;
+        this.antalRaderKod = 0;
+    }
+
+    public String getProgrammeringsSpråk() {
+        return programmeringsSpråk;
+    }
+
+    public void setProgrammeringsSpråk() {
+        programmeringsSpråk = this.programmeringsSpråk;
+    }
+
+    public void arbeta() {
+        antalRaderKod += 300;
+        System.out.println(namn + " skriver kod i " + programmeringsSpråk);
+    }
 }
